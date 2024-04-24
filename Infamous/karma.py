77b08e2234e4df4d9c9c19bcb453a89da1f3bc94
ZEOOXX44 <159913416+ZEOOX9210@users.noreply.github.com>
2024-04-24 12:40:58 +0530

    Update karma.py

--- Infamous/karma.py
+++ Infamous/karma.py
@@ -10,7 +10,7 @@ from Mikobot import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT
 
 # <============================================== CONSTANTS =========================================================>
 START_IMG = [
-    "https://telegra.ph/file/40b93b46642124605e678.jpg",
+    "https://graph.org/file/5237d82152f694964fba7.jpg",
     "https://telegra.ph/file/01a2e0cd1b9d03808c546.jpg",
     "https://telegra.ph/file/ed4385c26dcf6de70543f.jpg",
     "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
@@ -71,8 +71,8 @@ GROUP_START_BTN = [
 
 ALIVE_BTN = [
     [
-        ib(text="UPDATES", url="https://t.me/Hydra_Updates"),
-        ib(text="SUPPORT", url="https://t.me/hydraXsupport"),
+        ib(text="UPDATES", url="https://t.me/CHATTING_HUB8"),
+        ib(text="SUPPORT", url="https://t.me/CHATTING_HUB8"),
     ],
     [
         ib(
